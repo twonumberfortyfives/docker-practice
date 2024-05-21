7a4130d6ca77da6e41e8947b57b8962d2bf77f13
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-22 00:28:15 +0300

    docker volume and image files security

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.12-alpine
+FROM python:3.12-slim
 LABEL maintainer="sma4no.7@gmail.com"
 
 ENV PYTHOUNNBUFFERED 1
@@ -9,3 +9,15 @@ COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . .
+RUN mkdir -p /files/media
+
+RUN adduser \
+    --disabled-password \
+    --no-create-home \
+    my_user
+
+RUN chown -R my_user /files/media
+RUN chmod -R 755 /files/media
+
+
+USER my_user
--- product/models.py
+++ product/models.py
@@ -1,6 +1,16 @@
+import pathlib
+import uuid
+
 from django.db import models
+from django.utils.text import slugify
+
+
+def upload_image(instance, filename) -> pathlib.Path:
+    filename = (f"{slugify(instance.title)}-{uuid.uuid4()}" + pathlib.Path(filename).suffix)
+    return pathlib.Path("upload/products/") / pathlib.Path(filename)
 
 
 class Product(models.Model):
     title = models.CharField(max_length=64)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.ImageField(upload_to=upload_image, null=True)
